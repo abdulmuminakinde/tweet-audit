a0a3648af30b402703e3f1a8b3be8847a22495d3
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-12-24 01:18:55 +0100

    feat: implement initial core tweet analysis flow
    configure gemini client with detailed system instruction to audit tweet

--- cmd/process/main.go
+++ cmd/process/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/abdulmuminakinde/tweet-audit/internal/config"
 	"github.com/abdulmuminakinde/tweet-audit/internal/gemini"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter tweet to be analyzed: ")
+
+	question, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	trimmedQuestion := strings.TrimSpace(question)
+
 	ctx := context.Background()
 	cfg, err := config.LoadOrCreateConfig()
 	if err != nil {
@@ -21,7 +34,7 @@ func main() {
 		log.Fatal("error creating gemini client")
 	}
 
-	result, err := geminiclient.GenerateText(ctx)
+	result, err := geminiclient.GenerateText(ctx, trimmedQuestion)
 	if err != nil {
 		log.Fatalf("error creating client: %v", err)
 	}
